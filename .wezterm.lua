bb241547476f903252d8fa80ead40661917f8bd2
Gea-Suan Lin <gslin@gslin.org>
2024-03-12 02:28:59 +0800

    Update .wezterm.lua.

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,6 +7,7 @@ config.color_scheme = 'Tango (terminal.sexy)'
 config.disable_default_mouse_bindings = true
 config.font = wezterm.font_with_fallback({'Hack', 'Noto Sans CJK TC'})
 config.font_size = 18.0
+config.hide_mouse_cursor_when_typing = false
 config.initial_cols = 132
 config.initial_rows = 43
 config.window_padding = {
