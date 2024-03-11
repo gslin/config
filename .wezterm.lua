35bcc04cb91d67a5a125843a6a82c9fd9b5a1863
Gea-Suan Lin <gslin@gslin.org>
2024-03-12 01:35:56 +0800

    Add .wezterm.lua.

---
+++ .wezterm.lua
@@ -0,0 +1,19 @@
+--
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.check_for_updates = false
+config.color_scheme = 'Tango (terminal.sexy)'
+config.disable_default_mouse_bindings = true
+config.font = wezterm.font_with_fallback({'Hack', 'Noto Sans CJK TC'})
+config.font_size = 18.0
+config.initial_cols = 132
+config.initial_rows = 43
+config.window_padding = {
+    bottom = 0,
+    left = 0,
+    right = 0,
+    top = 0,
+}
+
+return config
